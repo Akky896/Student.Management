7bb34726da050c9f282a4493efc528dc507990d3
Akira Yamada <akira1356yama@icloud.com>
2026-05-29 14:24:09 +0900

    生徒情報の全件検索機能を追加

--- src/main/java/student/Student/Management/Application.java
+++ src/main/java/student/Student/Management/Application.java
@@ -1,14 +1,11 @@
 package student.Student.Management;
 
-import org.apache.ibatis.annotations.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 @RestController
@@ -22,7 +19,7 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
-	//生徒のデータ取得
+	//全生徒のデータ取得
 	@GetMapping("/students")
 	public List<Student> getAllStudent(){
 		return repository.findAll();
@@ -30,30 +27,19 @@ public class Application {
 
 
 	//生徒一人のデータ取得
-	@GetMapping("/student")
-	public String getStudent(@RequestParam String name){
-		Student student = repository.searchByName(name);
-		return student.getName() + " " +student.getAge() + "歳";
+	@GetMapping("/studentList")
+	public List<Student> getStudentList(){
+		return repository.search();
 	}
 
-	//生徒一人のデータ登録
-	@PostMapping("/student")
-	public void registerStudent(
-			String name,
-			int age){
-		repository.registerStudent(name,age);
+
+	//生徒のコース情報の取得
+	@GetMapping("/student_courseList")
+	public List<StudentCourse> getStudentsCourseList(){
+		return repository.searchCourse();
 	}
 
-	//生徒一人のデータ更新
-	@PatchMapping("/student")
-	public void updateStudentAge(String name, int age){
-		repository.updateStudent(name, age);
-	}
 
-	//生徒一人のデータ削除
-	@DeleteMapping("/student")
-	public void deleteStudent(String name){
-		repository.deleteStudent(name);
-	}
+
 
 }
--- src/main/java/student/Student/Management/StudentRepository.java
+++ src/main/java/student/Student/Management/StudentRepository.java
@@ -8,24 +8,19 @@ import java.util.List;
 public interface StudentRepository {
 
     //生徒一覧表示
-    @Select("SELECT * FROM student")
+    @Select("SELECT * FROM students")
     List<Student> findAll();
 
-    //生徒一人のデータ表示
-    @Select("SELECT * FROM student WHERE name = #{name}")
-    Student searchByName (String name);
+    //生徒のデータ表示
+    @Select("SELECT * FROM students")
+    List<Student> search();
 
-    //生徒一人のデータ登録
-    @Insert("INSERT student values(#{name}, #{age})")
-    void registerStudent(String name, int age);
+    //生徒の授業コース表示
+    @Select("SELECT * FROM student_courses")
+    List<StudentCourse> searchCourse();
 
-    //生徒一人のデータ更新
-    @Update("UPDATE student SET age = #{age} WHERE name = #{name}")
-    void updateStudent(String name, int age);
 
-    //生徒一人のデータ削除
-    @Delete("DELETE FROM student WHERE name = #{name}")
-    void deleteStudent(String name);
+
 
 
 }
